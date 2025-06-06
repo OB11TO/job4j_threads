e70f6fece050660c7c0616e0ce477e56f733c771
Artem Dagridjan <ob11to61@yandex.ru>
2025-06-06 18:14:08 +0300

    4. Thread без общих ресурсов [#267919]

---
+++ src/main/java/ru/job4j/immutable/ref/UserCacheDemo.java
@@ -0,0 +1,17 @@
+package ru.job4j.immutable.ref;
+
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class UserCacheDemo {
+    private final ConcurrentHashMap<Integer, User> users = new ConcurrentHashMap<>();
+    private final AtomicInteger id = new AtomicInteger();
+
+    public void add(User user) {
+        users.put(id.incrementAndGet(), user);
+    }
+
+    public User findById(int id) {
+        return users.get(id);
+    }
+}
