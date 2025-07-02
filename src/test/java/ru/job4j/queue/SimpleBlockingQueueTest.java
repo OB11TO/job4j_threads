5f62e331d4c64b31abc1605939fef9173f9c7659
Artem Dagridjan <ob11to61@yandex.ru>
2025-07-02 17:11:56 +0300

    fix

--- src/test/java/ru/job4j/queue/SimpleBlockingQueueTest.java
+++ src/test/java/ru/job4j/queue/SimpleBlockingQueueTest.java
@@ -135,53 +135,6 @@ class SimpleBlockingQueueTest {
     }
 
     @Test
-    public void whenFetchAllThenGetIt() {
-        final CopyOnWriteArrayList<Integer> buffer = new CopyOnWriteArrayList<>();
-        CountDownLatch latch = new CountDownLatch(2);
-        final SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<>();
-        Thread producer = new Thread(
-                () -> {
-                    IntStream.range(0, 5).forEach(elm -> {
-                        try {
-                            queue.offer(elm);
-                        } catch (InterruptedException e) {
-                            Thread.currentThread().interrupt();
-                            fail("Producer был прерван");
-                        } finally {
-                            latch.countDown();
-                        }
-                    });
-                }, "Producer"
-        );
-        producer.start();
-        Thread consumer = new Thread(
-                () -> {
-                    while (!queue.isEmpty() || !Thread.currentThread().isInterrupted()) {
-                        try {
-                            buffer.add(queue.poll());
-                        } catch (InterruptedException e) {
-                            Thread.currentThread().interrupt();
-                            fail("Consumer был прерван");
-                        } finally {
-                            latch.countDown();
-                        }
-                    }
-                }, "Consumer"
-        );
-        consumer.start();
-        try {
-            boolean completed = latch.await(1, TimeUnit.SECONDS);
-            assertThat(completed)
-                    .isTrue();
-        } catch (InterruptedException e) {
-            Thread.currentThread().interrupt();
-            fail("Тест прерван во время ожидания latch");
-        }
-        consumer.interrupt();
-        assertThat(buffer).containsExactly(0, 1, 2, 3, 4);
-    }
-
-    @Test
     void whenParallelProducerAndConsumerThenAllElementsConsumed() {
         SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<>();
         CopyOnWriteArrayList<Integer> buffer = new CopyOnWriteArrayList<>();
